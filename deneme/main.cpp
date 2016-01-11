6c461ec4edf1ccd0d4627f216ccc999f7c900063
adnansenyurt <adnansenyurt@superonline.com>
2016-01-11 14:22:51 +0200

    Initial Commit

---
+++ deneme/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  deneme
+//
+//  Created by Adnan Şenyurt on 11.01.2016.
+//  Copyright © 2016 Adnan Şenyurt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
